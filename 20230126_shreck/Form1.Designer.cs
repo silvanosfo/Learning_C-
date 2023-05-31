d116499abce24b02cb5527e25d0cb59a704bce58
Silvano <113552312+silvanosfo@users.noreply.github.com>
2023-05-31 22:21:30 +0100

    Add files via upload

---
+++ 20230126_shreck/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace _20230126_shreck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblAnoNasc = new System.Windows.Forms.Label();
+            this.lblValorBase = new System.Windows.Forms.Label();
+            this.lblDistrito = new System.Windows.Forms.Label();
+            this.lblConcelho = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtAnoNasc = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.txtValorBase = new System.Windows.Forms.TextBox();
+            this.chkDesc10 = new System.Windows.Forms.CheckBox();
+            this.chkDesc5 = new System.Windows.Forms.CheckBox();
+            this.chkAgrav12 = new System.Windows.Forms.CheckBox();
+            this.rdbSim = new System.Windows.Forms.RadioButton();
+            this.rdbNao = new System.Windows.Forms.RadioButton();
+            this.picShreck = new System.Windows.Forms.PictureBox();
+            this.cbxConcelhos = new System.Windows.Forms.ComboBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picShreck)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Location = new System.Drawing.Point(172, 24);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(35, 13);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "label1";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(51, 70);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(35, 13);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "label2";
+            // 
+            // lblAnoNasc
+            // 
+            this.lblAnoNasc.AutoSize = true;
+            this.lblAnoNasc.Location = new System.Drawing.Point(51, 107);
+            this.lblAnoNasc.Name = "lblAnoNasc";
+            this.lblAnoNasc.Size = new System.Drawing.Size(35, 13);
+            this.lblAnoNasc.TabIndex = 2;
+            this.lblAnoNasc.Text = "label3";
+            // 
+            // lblValorBase
+            // 
+            this.lblValorBase.AutoSize = true;
+            this.lblValorBase.Location = new System.Drawing.Point(51, 147);
+            this.lblValorBase.Name = "lblValorBase";
+            this.lblValorBase.Size = new System.Drawing.Size(35, 13);
+            this.lblValorBase.TabIndex = 3;
+            this.lblValorBase.Text = "label4";
+            // 
+            // lblDistrito
+            // 
+            this.lblDistrito.AutoSize = true;
+            this.lblDistrito.Location = new System.Drawing.Point(51, 254);
+            this.lblDistrito.Name = "lblDistrito";
+            this.lblDistrito.Size = new System.Drawing.Size(35, 13);
+            this.lblDistrito.TabIndex = 4;
+            this.lblDistrito.Text = "label5";
+            // 
+            // lblConcelho
+            // 
+            this.lblConcelho.AutoSize = true;
+            this.lblConcelho.Location = new System.Drawing.Point(352, 254);
+            this.lblConcelho.Name = "lblConcelho";
+            this.lblConcelho.Size = new System.Drawing.Size(35, 13);
+            this.lblConcelho.TabIndex = 5;
+            this.lblConcelho.Text = "label6";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(180, 70);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 20);
+            this.txtNome.TabIndex = 6;
+            // 
+            // txtAnoNasc
+            // 
+            this.txtAnoNasc.Location = new System.Drawing.Point(180, 104);
+            this.txtAnoNasc.Name = "txtAnoNasc";
+            this.txtAnoNasc.Size = new System.Drawing.Size(100, 20);
+            this.txtAnoNasc.TabIndex = 7;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(430, 318);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(100, 20);
+            this.txtResultado.TabIndex = 9;
+            // 
+            // txtValorBase
+            // 
+            this.txtValorBase.Location = new System.Drawing.Point(180, 140);
+            this.txtValorBase.Name = "txtValorBase";
+            this.txtValorBase.Size = new System.Drawing.Size(100, 20);
+            this.txtValorBase.TabIndex = 10;
+            // 
+            // chkDesc10
+            // 
+            this.chkDesc10.AutoSize = true;
+            this.chkDesc10.Location = new System.Drawing.Point(180, 171);
+            this.chkDesc10.Name = "chkDesc10";
+            this.chkDesc10.Size = new System.Drawing.Size(80, 17);
+            this.chkDesc10.TabIndex = 11;
+            this.chkDesc10.Text = "checkBox1";
+            this.chkDesc10.UseVisualStyleBackColor = true;
+            // 
+            // chkDesc5
+            // 
+            this.chkDesc5.AutoSize = true;
+            this.chkDesc5.Location = new System.Drawing.Point(180, 194);
+            this.chkDesc5.Name = "chkDesc5";
+            this.chkDesc5.Size = new System.Drawing.Size(80, 17);
+            this.chkDesc5.TabIndex = 12;
+            this.chkDesc5.Text = "checkBox2";
+            this.chkDesc5.UseVisualStyleBackColor = true;
+            // 
+            // chkAgrav12
+            // 
+            this.chkAgrav12.AutoSize = true;
+            this.chkAgrav12.Location = new System.Drawing.Point(180, 217);
+            this.chkAgrav12.Name = "chkAgrav12";
+            this.chkAgrav12.Size = new System.Drawing.Size(80, 17);
+            this.chkAgrav12.TabIndex = 13;
+            this.chkAgrav12.Text = "checkBox3";
+            this.chkAgrav12.UseVisualStyleBackColor = true;
+            // 
+            // rdbSim
+            // 
+            this.rdbSim.AutoSize = true;
+            this.rdbSim.Location = new System.Drawing.Point(175, 250);
+            this.rdbSim.Name = "rdbSim";
+            this.rdbSim.Size = new System.Drawing.Size(85, 17);
+            this.rdbSim.TabIndex = 14;
+            this.rdbSim.TabStop = true;
+            this.rdbSim.Text = "radioButton1";
+            this.rdbSim.UseVisualStyleBackColor = true;
+            // 
+            // rdbNao
+            // 
+            this.rdbNao.AutoSize = true;
+            this.rdbNao.Location = new System.Drawing.Point(234, 250);
+            this.rdbNao.Name = "rdbNao";
+            this.rdbNao.Size = new System.Drawing.Size(85, 17);
+            this.rdbNao.TabIndex = 15;
+            this.rdbNao.TabStop = true;
+            this.rdbNao.Text = "radioButton2";
+            this.rdbNao.UseVisualStyleBackColor = true;
+            // 
+            // picShreck
+            // 
+            this.picShreck.Location = new System.Drawing.Point(355, 70);
+            this.picShreck.Name = "picShreck";
+            this.picShreck.Size = new System.Drawing.Size(175, 112);
+            this.picShreck.TabIndex = 16;
+            this.picShreck.TabStop = false;
+            // 
+            // cbxConcelhos
+            // 
+            this.cbxConcelhos.FormattingEnabled = true;
+            this.cbxConcelhos.Location = new System.Drawing.Point(430, 245);
+            this.cbxConcelhos.Name = "cbxConcelhos";
+            this.cbxConcelhos.Size = new System.Drawing.Size(100, 21);
+            this.cbxConcelhos.TabIndex = 17;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(336, 316);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 18;
+            this.btnCalc.Text = "button1";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 350);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.cbxConcelhos);
+            this.Controls.Add(this.picShreck);
+            this.Controls.Add(this.rdbNao);
+            this.Controls.Add(this.rdbSim);
+            this.Controls.Add(this.chkAgrav12);
+            this.Controls.Add(this.chkDesc5);
+            this.Controls.Add(this.chkDesc10);
+            this.Controls.Add(this.txtValorBase);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtAnoNasc);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblConcelho);
+            this.Controls.Add(this.lblDistrito);
+            this.Controls.Add(this.lblValorBase);
+            this.Controls.Add(this.lblAnoNasc);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picShreck)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblAnoNasc;
+        private System.Windows.Forms.Label lblValorBase;
+        private System.Windows.Forms.Label lblDistrito;
+        private System.Windows.Forms.Label lblConcelho;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtAnoNasc;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.TextBox txtValorBase;
+        private System.Windows.Forms.CheckBox chkDesc10;
+        private System.Windows.Forms.CheckBox chkDesc5;
+        private System.Windows.Forms.CheckBox chkAgrav12;
+        private System.Windows.Forms.RadioButton rdbSim;
+        private System.Windows.Forms.RadioButton rdbNao;
+        private System.Windows.Forms.PictureBox picShreck;
+        private System.Windows.Forms.ComboBox cbxConcelhos;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ 20230126_shreck/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _20230126_shreck
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblTitulo.Text = "CÁLCULO DO VALOR DA AVENÇA";
+            lblNome.Text = "NOME";
+            lblAnoNasc.Text = "ANO DE NASCIMENTO";
+            lblValorBase.Text = "VALOR BASE";
+            lblDistrito.Text = "Reside no distrito?";
+            lblConcelho.Text = "CONCELHO";
+            chkDesc10.Text = "Desconto de escalão? (10%)";
+            chkDesc5.Text = "Desconto de sócio? (5%)";
+            chkAgrav12.Text = "Tem agravamento? (12€)";
+            rdbSim.Text = "Sim";
+            rdbNao.Text = "Não";
+            picShreck.Load("https://steamuserimages-a.akamaihd.net/ugc/285225308508782583/ACF1F2CCC07EF721673F913C9848ECD84D147E4F/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false");
+            picShreck.SizeMode = PictureBoxSizeMode.StretchImage;
+            btnCalc.Text = "CALCULAR";
+            cbxConcelhos.Items.Add("Braga");
+            cbxConcelhos.Items.Add("Amares");
+            cbxConcelhos.Items.Add("Guimarães");
+            txtValorBase.Text = "100";
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double descontos = 0;
+            double resultado = Convert.ToInt64(txtValorBase.Text);
+            double valorbase = Convert.ToInt64(txtValorBase.Text);
+
+            if (Convert.ToInt32(txtAnoNasc.Text) >= 2000)
+            {
+                descontos += valorbase * 0.1;
+            }
+
+            if (chkDesc10.Checked)
+            {
+                descontos += valorbase * 0.1;
+            }
+
+            if (chkDesc5.Checked)
+            {
+                descontos += valorbase * 0.05;
+            }
+
+            if (chkAgrav12.Checked)
+            {
+                descontos -= 12;
+            }
+
+            if (rdbSim.Checked)
+            {
+                descontos += valorbase * 0.01;
+            }
+
+            if (cbxConcelhos.Text == "Amares")
+            {
+                descontos += valorbase * 0.2;
+            }
+
+            resultado -= descontos;
+            txtResultado.Text = Convert.ToString(resultado);
+        }
+    }
+}
